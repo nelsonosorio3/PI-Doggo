fb1e360a6c8ce70894794811590024bfaacb8130
nelson <nelsonsorio@hotmail.com>
2021-08-21 19:24:52 -0500

    minor corrections css style

--- client/src/components/bottomBar/bottomBar.jsx
+++ client/src/components/bottomBar/bottomBar.jsx
@@ -1,39 +1,17 @@
 import React, {useState, useEffect} from "react";
 import Pagination from "./pagination.jsx";
 import { connect } from "react-redux";
-import { goN } from "../../actions";
 import "./bottomBar.css"
 
 export function BottomBar(breed) {
-
-  const amountPages = Math.ceil(breed.breed.results.length/8) || 1;
-  const handleClick = function(number){
-    // setCurrentPage(number)
-    breed.goN(number);
-  };
-
-  useEffect(()=>{
-  
-  });
   return(
 
     <div id="bottomBar">
-      {/* <div>
-        <button className="bottombuttons" onClick={() => handleClick(1)}>First Page</button>
-      </div>
-      <div>
-      <button className="bottombuttons" onClick={() => handleClick(currentPage-1)}>Previous Page</button>
-      </div> */}
-      
-        <Pagination/>
+      {
+        breed.breed.results.length? <Pagination/>: <div></div>
+      }
+      {/* <Pagination/> */}
     
-      {/* <div>
-      <button className="bottombuttons"  onClick={() => handleClick(currentPage+1)}>Next Page</button>
-      </div>
-      <div>
-      <button className="bottombuttons" onClick={() => handleClick(amountPages)}>Last Page </button>
-      </div> */}
-      
     </div>
   )
 }
@@ -44,10 +22,6 @@ function mapStateToprops(state){
   };
 };
 
-function mapDispatchToProps(dispatch){
-  return{
-    goN: breed => dispatch(goN(breed))
-  };
-};
 
-export default connect(mapStateToprops, mapDispatchToProps)(BottomBar);
+
+export default connect(mapStateToprops)(BottomBar);
