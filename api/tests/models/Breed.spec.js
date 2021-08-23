fe2c5821b3b5211c48e09f43c5df070ebf99ab57
nelson <nelsonsorio@hotmail.com>
2021-08-23 16:41:38 -0500

    more tests

--- api/tests/models/Breed.spec.js
+++ api/tests/models/Breed.spec.js
@@ -1,4 +1,4 @@
-const { Breed, conn } = require('../../src/db.js');
+const { Breed, Temperament, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
 describe('Breed model', () => {
@@ -17,6 +17,34 @@ describe('Breed model', () => {
       it('should work when its a valid name', () => {
         Breed.create({ name: 'Pug' });
       });
+      it("should not duplicate already existing name", ()=> {
+        Breed.create({name: "pug"});
+        Breed.create({name: "pug"});
+      })
+    });
+  });
+});
+
+describe('Temperament model', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators', () => {
+    beforeEach(() => Temperament.sync({ force: true }));
+    describe('name', () => {
+      it('should throw an error if name is null', (done) => {
+        Breed.create({})
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
+      });
+      it('should work when its a valid name', () => {
+        Breed.create({ name: 'Hyper-active' });
+      });
+      it("should not duplicate already existing name", ()=> {
+        Breed.create({name: "Hyper-active"});
+        Breed.create({name: "Hyper-active"});
+      })
     });
   });
 });
--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -7,6 +7,9 @@ const { Dog, Breed, Temperament, conn } = require('../../src/db.js');
 const agent = session(app);
 const dog = {
   name: 'Pug',
+  height: "2 - 6",
+  weight: "20 - 25", 
+  life_span:"10 - 12"
 };
 
 describe('Breed routes', () => {
@@ -14,24 +17,36 @@ describe('Breed routes', () => {
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Dog.sync({ force: true })
-    .then(() => Dog.create(dog)));
+  beforeEach(() => Breed.sync({ force: true })
+    .then(() => Breed.create(dog)));
   describe('GET /dogs', () => {
     it('should get 200', () =>
       agent.get('/dogs').expect(200)
     );
   });
+  describe('GET /dogs/:id', () => {
+    it('should get 200', () =>
+      agent.get('/dogs/1').expect(200)
+    );
+    it("should get 404 when id does not exists in database", ()=>
+      agent.get("/dogs/22553").expect(404)
+    )
+  });
+  describe("Get /dogs?name=Pug", () => {
+    it("should get 200", () =>
+    agent.get("/dogs?name=Pug").expect(200))
+  })
   describe("Post /dog", ()=> {
     it("should add dog", ()=>{
       agent.post("/dog").send({name: "nuevo perro", height: "2-5", weight: "20 - 25", life_span:"10 - 12"}).expect(200)
     })
     it("should not add a dog, if it already exist in the database", ()=>{
       agent.post("/dog").send({name: "nuevo perro", height: "2-5", weight: "20 - 25", life_span:"10 - 12"})
-      agent.post("/dog").send({name: "Pug", height: "2-5", weight: "20 - 25", life_span:"10 - 12"}).expect(200, "That breead already exists in the database")
+      agent.post("/dog").send({name: "nuevo perro", height: "2-5", weight: "20 - 25", life_span:"10 - 12"}).expect(200, "That breead already exists in the database")
     })
   })
-  describe("Post /dog", ()=> {
-    it("shoult get temperaments in database", ()=>{
+  describe("Get /dog", ()=> {
+    it("should get temperaments in database", ()=>{
       agent.get("/temperament").expect(200)
     })
   })
